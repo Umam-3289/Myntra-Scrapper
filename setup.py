d758f13518e86c45e4b6023ec96cdc5cd5dbdf0e
Um-3289 <umamkhan9931@gmail.com>
2025-07-30 18:23:06 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name='Myntra Scrapper',
+    version='1.0.0',
+    author="Umam Khan",
+    author_email='umamkhan9931@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
