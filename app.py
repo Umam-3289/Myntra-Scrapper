33f063920435adcb3ab3930c553106b75ec31a32
Um-3289 <umamkhan9931@gmail.com>
2025-08-01 16:00:40 +0530

    all neccessary files build

---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st
+from src.cloud_io import MongoIO
+from src.constants import SESSION_PRODUCT_KEY
+from src.scrapper.scrape import ScrapReviews
+
+st.set_page_config(
+    "myntra-review-scrapper"
+)
+
+st.tittle("Myntra Review Scrapper")
+st.session_state["data"]=False
+
+def form_input():
+    product=st.text_input("Search Products")
+    st.session_state[SESSION_PRODUCT_KEY]=product
+    no_of_products=st.number_input("No of Products to search",step=1,min_value=1)
+
+    if st.button("Scrape Reviews"):
+        scrapper=ScrapReviews(
+            product_name=product,
+            no_of_products=int(no_of_products)
+        )
+    
+    scrapped_data=scrapper.get_review_data()
+    if scrapped_data is not None:
+        st.session_state["data"]=True
+        mongoio=MongoIO()
+        mongoio.store_reviews(product_name=product,reviews=scrapped_data)
+        print("Stored data into MongoDB")
+    st.dataframe(scrapped_data)
+
+if __name__=="main":
+    data=form_input()
---
+++ src/constants/__init__.py
@@ -0,0 +1,4 @@
+MONGODB_URL_KEY: str="mongodb+srv://Umam:12345@cluster0.9ajxfcx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str="myntra-reviews"
+
+SESSION_PRODUCT_KEY: str="product_name"
